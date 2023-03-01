f2599753d5ae23d3f6f419e36fcab1721218ec51
Nilton Rocha <57201440+niltonrochadeveloper@users.noreply.github.com>
2023-03-01 12:51:19 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,7 +64,7 @@ export default function Index() {
 
       <div style={{width: '100vw', height: '100vh', justifyContent: 'center', alignItems: 'center'}}>
         { isLoading && <Loading /> }
-        { user.lenght > 0 && <Card username={user[0].username} /> }
+        { user.length > 0 && <Card username={user[0].username} /> }
       </div>
 
     </div>
