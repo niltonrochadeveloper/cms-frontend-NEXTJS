d0df82836406f3efdb340c42e81d84e036cc83f7
Nilton Rocha <nilton.silva@grupocard.com.br>
2023-03-01 15:25:06 -0300

    atualização de código

---
+++ hooks/apiCms.tsx
@@ -0,0 +1,17 @@
+export async function Auth() {
+
+  const response = {
+    id: 1,
+    role: 1,
+    username: "Nilton Rocha",
+    password: "123@Qwe",
+    status: 200,
+}
+  const result = response
+
+  return {
+    response,
+    result
+  }
+
+}
